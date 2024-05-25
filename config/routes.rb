d94c3dd4c530d628f880d446a408b2a97754420f
github akari07voi <ymytxxo29@yahoo.co.jp>
2024-05-25 12:41:48 +0900

    商品一覧表示機能実装※slimでレイアウト崩れがあったため微調整含

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :tasks
+  root to:'items#index'
+
+  get 'items/index'
+  get 'items/show'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Item.create(
+#   name: 'Tシャツ',
+#   description: '白い無地のTシャツです。',
+#   price: 1000
+# )
+
+# Item.create(
+#   name: 'パーカー',
+#   description: '白い無地のパーカーです。',
+#   price: 1500
+# )
+
+Item.create(
+  name: 'ソックス',
+  description: '黒い無地のソックスです。',
+  price: 500
+)
