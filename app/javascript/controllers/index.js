8ddc5475a02556b57ad72705342a0a394d869745
github akari07voi <ymytxxo29@yahoo.co.jp>
2024-05-27 22:56:35 +0900

    ポート番号設定修正（index.jsにHerokuの設定ポートを取得するよう記述追加）

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,14 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 3000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
