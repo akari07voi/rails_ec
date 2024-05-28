c2c54a91884122cab3ee53754595ea6c5bfd618b
github akari07voi <ymytxxo29@yahoo.co.jp>
2024-05-28 18:51:53 +0900

    ポート番号の設定を動的取得でなく固定3000に戻し

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: unset PORT && bin/rails server -p $PORT
-js: yarn build --watch
-css: yarn build:css --watch
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,11 +7,3 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-const PORT = process.env.PORT || 3000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
