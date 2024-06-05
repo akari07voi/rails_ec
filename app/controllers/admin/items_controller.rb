08e26ed8f148c592c4539b7c76100b150dbf9996
github akari07voi <ymytxxo29@yahoo.co.jp>
2024-06-05 20:26:39 +0900

    管理機能追加（ログイン機能未実装）

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,37 @@
+class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.with_attached_image.order(:id)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    item.save!
+    redirect_to items_url, notice: "商品名「#{item.name}」を登録しました"
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update!(item_params)
+    redirect_to items_url, notice: "商品名「#{item.name}」を更新しました"
+  end
+
+  def destroy
+    item = Item.find(:id)
+    item.destroy
+    redirect_to admin_items_url, notice: "商品名「#{item.name}」を削除しました"
+  end
+
+  private
+
+  def items_params
+    params.riquire(:item).permit(:name, :price, :description, :image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :items
+    # get 'items/index'
+    # get 'items/new'
+    # get 'items/edit'
+    delete 'items/:id', to: 'items#destroy'
+  end
   root to: 'items#index'
 
   resources :items
