d8824a0bf1f9717824433e11183ceb4f217dc834
github akari07voi <ymytxxo29@yahoo.co.jp>
2024-06-08 22:57:29 +0900

    turbo有効化

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 # Pin npm packages by running ./bin/importmap
-pin '@hotwired/turbo-rails', to: 'turbo.js'
 pin 'application'
+pin '@hotwired/turbo-rails', to: 'turbo.min.js'
+pin '@hotwired/stimulus', to: 'stimulus.min.js'
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin_all_from 'app/javascript/controllers', under: 'controllers'
