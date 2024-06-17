291d4d44554c7dea14a082a2486b7ea7820983dd
github akari07voi <ymytxxo29@yahoo.co.jp>
2024-06-17 19:07:12 +0900

    Cartテーブル検索時のセッション内の項目指定タイポ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_cart
-    @cart = Cart.find_or_create_by(id: session[:cart])
+    @cart = Cart.find_or_create_by(id: session[:cart_id])
     @cart_items = @cart.cart_items
     session[:cart_id] = @cart.id
   end
