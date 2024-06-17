c613b478a1433c7239638f7fd933554e3280bfa9
github akari07voi <ymytxxo29@yahoo.co.jp>
2024-06-17 18:59:56 +0900

    CartsControllerでのアクションメソッドでテーブルでの検索をせずインスタンス変数の中身を参照するよう修正、繰り返し処理の記述を修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,15 +2,12 @@
 
 class CartsController < ApplicationController
   def index
-    session[:cart_detail] =
-      CartItem.where(cart_id: session[:cart]).group(:item_id).count
-    @cart_detail = session[:cart_detail]
-    return if @cart_detail.nil?
+    @cart_detail = @cart_items.group(:item_id).count
 
     @items = Item.where(id: @cart_detail.keys)
     @all_price = 0
-    @items.each do |item|
-      @all_price += item.price * @cart_detail[item.id]
+    @cart_items.each do |cart_item|
+      @all_price += cart_item.item.price
     end
   end
 
@@ -18,17 +15,16 @@ class CartsController < ApplicationController
 
   def create
     @item = Item.find(params[:item_id])
-    add_cart_item = []
     add_count = params[:count] ? params[:count].to_i : 1
-    (1..add_count).each do
-      add_cart_item << CartItem.new(item_id: @item.id, cart_id: session[:cart])
+    add_cart_item = (1..add_count).map do
+      @cart.cart_items.build(item_id: @item.id)
     end
     CartItem.import add_cart_item
     redirect_to root_path
   end
 
   def destroy
-    destroy_item = CartItem.where(cart_id: session[:cart], item_id: params[:item_id])
+    destroy_item = @cart_item.where(item_id: params[:item_id])
     destroy_item.destroy_all
     redirect_to carts_path
   end
