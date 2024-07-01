5f61493b299e5a693149664a304d891ed1f24969
github akari07voi <ymytxxo29@yahoo.co.jp>
2024-07-01 18:50:26 +0900

    開発用環境のメール送信設定を修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
     port: 587,
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD'],
+    user_name: 'exaount712ak@gmail.com',
+    password: 'jdgxcttvwjripljh',
     authentication: 'login',
     enable_starttls_auto: true
   }
