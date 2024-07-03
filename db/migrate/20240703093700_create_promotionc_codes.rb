d4f7293caf7cc6e8711c3582d9b81a68a00ea0e7
github akari07voi <ymytxxo29@yahoo.co.jp>
2024-07-03 19:20:50 +0900

    プロモーションコード生成rake task実装

---
+++ db/migrate/20240703093700_create_promotionc_codes.rb
@@ -0,0 +1,11 @@
+class CreatePromotioncCodes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :promotionc_codes do |t|
+      t.string :code
+      t.datetime :used_at
+      t.integer :educed_price
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_28_120136) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_03_093700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,6 +85,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_28_120136) do
   end
 
   create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "promotionc_codes", force: :cascade do |t|
+    t.string "code"
+    t.datetime "used_at"
+    t.integer "educed_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
